8b901b42c73222e8f6cea0b115193c9ddddc77c3
abuzar-hindi <hindiabuzar@gmail.com>
2026-02-15 13:34:26 +0530

    changed in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,21 +11,7 @@ const Contact = () => {
       </div>
 
       <div className="my-10 mb-20">
-        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
-          <div className="bg-white space-y-4 rounded-lg shadow-md overflow-hidden">
-            <iframe
-              title="map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227994.67311142205!2d81.70666259453125!3d26.762981900000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399a1bf14809764f%3A0xaf141c8d42e5af2b!2sMannat%20darbaar!5e0!3m2!1sen!2sin!4v1770051880104!5m2!1sen!2sin"
-              className="w-full h-64 md:h-full border-0"
-            />
-
-            <iframe
-              title="map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227994.67311142205!2d81.70666259453125!3d26.762981900000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399a1bf14809764f%3A0xaf141c8d42e5af2b!2sMannat%20darbaar!5e0!3m2!1sen!2sin!4v1770051880104!5m2!1sen!2sin"
-              className="w-full h-64 md:h-full border-0"
-            />
-          </div>
-
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-stretch">
           <div className="bg-white rounded-lg shadow-md p-8">
             <p className="font-semibold text-xl text-[#FEA116]">
               Our Restaurant
@@ -64,6 +50,20 @@ const Contact = () => {
                 </button>
               </Link>
             </div>
+          </div>
+
+          <div className="bg-white space-y-2 rounded-lg shadow-md overflow-hidden flex flex-col min-h-[420px] md:min-h-full">
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227994.67311142205!2d81.70666259453125!3d26.762981900000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399a1bf14809764f%3A0xaf141c8d42e5af2b!2sMannat%20darbaar!5e0!3m2!1sen!2sin!4v1770051880104!5m2!1sen!2sin"
+              className="w-full h-64 md:h-full border-0"
+            />
+
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227994.67311142205!2d81.70666259453125!3d26.762981900000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399a1bf14809764f%3A0xaf141c8d42e5af2b!2sMannat%20darbaar!5e0!3m2!1sen!2sin!4v1770051880104!5m2!1sen!2sin"
+              className="w-full h-64 md:h-full border-0"
+            />
           </div>
         </div>
       </div>
